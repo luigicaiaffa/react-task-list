20b8981ed3e6c1c20b0a62c30009714e383c899c
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-27 15:07:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { tasks } from "./data/tasks";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>Task Manager</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
